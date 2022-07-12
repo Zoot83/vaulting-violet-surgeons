f16646cfa28bf516901bc427ff7d7ec76f76a4b5
nickmaier <nicholasamaier@gmail.com>
2022-07-12 19:13:44 -0400

    Added first pieces of javascript - Nick

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,13 @@
+var nameTextBox = document.getElementById("#nameTextBox");
+var calorieGoalTextBox = document.getElementById("#calorieGoalTextBox");
+var submitBTn = document.getElementById("#submitBtn");
 
+submitBTn.addEventListener("click", submition);
+
+
+function submition () {
+    var userName = nameTextBox.value;
+    var calorieGoal = calorieGoalTextBox.value;
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("calorieGoal", calorieGoal);
+}
